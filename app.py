951453ef6801724028af200a1387cb72db515d20
Oswaldtan1020 <oswaldtan1020@gmail.com>
2026-06-02 23:15:58 +0800

    Prepare Flask app for Render deployment

--- app.py
+++ app.py
@@ -16,5 +16,12 @@ app.register_blueprint(readings_api, url_prefix="/api")
 # 🔥 DEBUG ROUTES
 print(app.url_map)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
