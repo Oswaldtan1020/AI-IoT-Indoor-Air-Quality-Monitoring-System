bb27c86bb68a7cc65f3f3264247a7d98c7b24626
Oswaldtan1020 <oswaldtan1020@gmail.com>
2026-06-04 10:33:55 +0800

    Add insert API route

--- api/insert_api.py
+++ api/insert_api.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from backend.database import get_db
+from datetime import datetime, timedelta
 
 insert_api = Blueprint("insert_api", __name__)
 
@@ -8,19 +9,22 @@ def insert_reading():
     try:
         data = request.json
 
+        malaysia_time = datetime.utcnow() + timedelta(hours=8)
+
         conn = get_db()
         cursor = conn.cursor()
 
         cursor.execute("""
             INSERT INTO readings
-            (room_id, temperature, humidity, co2, pm25)
-            VALUES (%s, %s, %s, %s, %s)
+            (room_id, temperature, humidity, co2, pm25, timestamp)
+            VALUES (%s, %s, %s, %s, %s, %s)
         """, (
             data["room_id"],
             data["temperature"],
             data["humidity"],
             data["co2"],
-            data["pm25"]
+            data["pm25"],
+            malaysia_time
         ))
 
         conn.commit()
